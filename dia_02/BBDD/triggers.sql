cd03bdb29d359b5e4ac76b7acea781aef9c35f34
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-09-09 18:26:40 +0000

    feat: :sparkles: Agregar trigger

--- dia_02/BBDD/triggers.sql
+++ dia_02/BBDD/triggers.sql
@@ -1,13 +1,43 @@
-CREATE OR REPLACE FUNCTION insert_regions()
+CREATE OR REPLACE FUNCTION split_and_deduplicate()
 RETURNS TRIGGER AS $$
+DECLARE
+    v_region_id INT;
+    v_departamento_id INT;
+    v_municipio_id INT;
 BEGIN
-    INSERT INTO regiones (region)
-    VALUES (NEW.region);
-    RETURN NEW;
+    -- Verificar e insertar la región si no existe
+    SELECT id_region INTO v_region_id
+    FROM regiones
+    WHERE region = NEW.region;
+
+    IF NOT FOUND THEN
+        INSERT INTO regiones (region)
+        VALUES (NEW.region)
+        RETURNING id_region INTO v_region_id;
+    END IF;
+
+    -- Verificar e insertar el departamento si no existe
+    SELECT id_departamento INTO v_departamento_id
+    FROM departamentos
+    WHERE id_region = v_region_id
+      AND departamento = NEW.departamento;
+
+    IF NOT FOUND THEN
+        INSERT INTO departamentos (id_region, departamento)
+        VALUES (v_region_id, NEW.departamento)
+        RETURNING id_departamento INTO v_departamento_id;
+    END IF;
+
+   -- Verificar e insertar el departamento si no existe
+    INSERT INTO municipios (id_departamento, municipio)
+    VALUES (v_departamento_id, NEW.municipio)
+    ON CONFLICT (id_departamento, municipio) DO NOTHING;
+
+    RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TRIGGER regions_into_table
-AFTER INSERT OR UPDATE ON regiones_municipios_departamentos
+CREATE TRIGGER sort_info
+AFTER INSERT ON regiones_municipios_departamentos
 FOR EACH ROW
-EXECUTE FUNCTION insert_regions();
+EXECUTE FUNCTION split_and_deduplicate();
