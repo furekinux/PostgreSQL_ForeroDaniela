bc0a1eebb5b53f929a2abc86f3fab3d284c66481
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-09-09 14:06:23 +0000

    feat: :sparkles: Agregar y modificar tablas

--- dia_02/BBDD/tables.sql
+++ dia_02/BBDD/tables.sql
@@ -1,16 +1,23 @@
 create table personas(
-	nombre varchar(50) not null,
-	apellido varchar(120) not null,
-	municipio_nacimiento varchar(200) not null,
-	municipio_domicilio varchar(200) not null
+	id_persona serial primary key,
+	nombre text not null,
+	apellido text not null,
+	municipio_nacimiento text not null,
+	municipio_domicilio text not null
+);
+
+create table regiones_municipios(
+	id_regiones serial primary key,
+	region text not null,
+	departamento text not null,
+	codigo_departamento text not null,
+	municipio text not null,
+	codigo_municipio text not null
 );
 
 create table regiones(
-	id_persona serial primary key,
-	nombre varchar(50) not null,
-	apellido varchar(120) not null,
-	municipio_nacimiento varchar(200) not null,
-	municipio_domicilio varchar(200) not null
+	id_region serial primary key,
+	region text not null
 );
 
 SELECT * FROM pg_catalog.pg_tables;
